8e7619298d173d05b8e33efd066b0dfd71f67b65
BishwajeetS11 <bishwajeetsahoo11@gmail.com>
2026-07-29 14:06:38 +0530

    Updated greeting on submission branch

--- Assignment_TW1.1_Git_Workflow/hello-flask/app.py
+++ Assignment_TW1.1_Git_Workflow/hello-flask/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("User Authentication Feature")
-    return "Hello World"
+    return "Hello World from Main branch!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
